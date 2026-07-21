d1a9d8a6acc293d088074e9f78653c6aa25308d4
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-07-21 22:03:18 +0530

    feat(api-gateway): initialize API gateway service with basic structure and configurations

--- services/config-server/src/main/java/com/sahyog/config_server/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/sahyog/config_server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.sahyog.configserver;
+package com.sahyog.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
